2734cdb24b6d65174f09d8c62046a4695673303e
Atcharapron Suksri <atcharapron.s@ku.th>
2026-09-18 16:13:25 +0700

    Add files via upload

--- ProductAnalytics.java
+++ ProductAnalytics.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.*;
 
 public class ProductAnalytics {
     private List<Product> productCatalog;
@@ -13,7 +14,14 @@ public class ProductAnalytics {
     /**
      * ค้นหาสินค้าทั้งหมดในหมวดหมู่ที่กำหนด
      */
-    public List<Product> findProductsByCategory(String category) {
+    public List<Product> findProductsByCategory(String category){
+        return productCatalog.stream()
+        .filter(p->p.category().equalsIgnoreCase(category))
+        .collect(Collectors.toList());
+
+    }
+
+    /*public List<Product> findProductsByCategory(String category) {
         List<Product> results = new ArrayList<>();
         for (Product p : productCatalog) {
             if (p.category().equalsIgnoreCase(category)) {
@@ -21,12 +29,18 @@ public class ProductAnalytics {
             }
         }
         return results;
-    }
+    }*/
 
     /**
      * คืนค่า "ชื่อ" ของสินค้าทั้งหมดที่มีราคาต่ำกว่าที่กำหนด
      */
-    public List<String> getProductNamesWithPriceLessThan(double maxPrice) {
+    public List<String> getProductNamesWithPriceLessThan(double maxPrice){
+        return productCatalog.stream()
+        .filter(p->p.price() < maxPrice)
+        .map(p->p.name())
+        .collect(Collectors.toList());
+    }
+    /*public List<String> getProductNamesWithPriceLessThan(double maxPrice) {
         List<String> results = new ArrayList<>();
         for (Product p : productCatalog) {
             if (p.price() < maxPrice) {
@@ -34,12 +48,21 @@ public class ProductAnalytics {
             }
         }
         return results;
-    }
+    }*/
 
     /**
      * คำนวณมูลค่ารวมของสต็อกสินค้าในหมวดหมู่ที่กำหนด
      */
-    public double calculateTotalStockValueForCategory(String category) {
+    public double calculateTotalStockValueForCategory(String category){
+        return productCatalog.stream()
+        .filter(p->p.category().equalsIgnoreCase(category))
+        .mapToDouble(p->p.price() * p.stock())
+        .sum();
+        
+    }
+
+
+    /*public double calculateTotalStockValueForCategory(String category) {
         double totalValue = 0.0;
         for (Product p : productCatalog) {
             if (p.category().equalsIgnoreCase(category)) {
@@ -47,17 +70,27 @@ public class ProductAnalytics {
             }
         }
         return totalValue;
-    }
+    }*/
 
     /**
      * ตรวจสอบว่ามีสินค้าที่หมดสต็อก (stock = 0) หรือไม่
      */
-    public boolean hasProductOutOfStock() {
+    public boolean hasProductOutOfStock(){
+        return productCatalog.stream()
+        .filter(p->p.stock()==0)
+        .count() > 0 ;
+
+        //return productCatalog.stream()
+        //.anyMatch(p->p.stock()==0) วิธีทำอีกวิธี
+    }
+
+
+    /*public boolean hasProductOutOfStock() {
         for (Product p : productCatalog) {
             if (p.stock() == 0) {
                 return true;
             }
         }
         return false;
-    }
+    }*/
 }
